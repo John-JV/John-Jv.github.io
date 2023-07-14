eff049f1357ae6ee0dad7412945d34fe1a140154
John-JV <83227972+John-JV@users.noreply.github.com>
2023-07-15 00:55:55 +0300

    Add files via upload

---
+++ Romec/onf.js
@@ -0,0 +1,19 @@
+function one(){
+    var x=document.getElementById('k')
+    var y=document.getElementById('i')
+    var description=document.getElementById('description')
+
+        if(x.className==='bdn' && y.className==='img'){
+                y.className='imgt'
+                x.className='bd'
+              
+            }
+        else{
+            x.className='bdn'
+            y.className='img'
+        }
+
+      
+
+    }
+    
